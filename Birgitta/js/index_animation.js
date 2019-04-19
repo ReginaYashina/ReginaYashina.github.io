f4dbb2f4adbd1fb5291bd453e94d549102d71006
ReginaYashina <reginayashina@inbox.ru>
2019-04-19 22:35:27 +0300

    add second-index-page

--- Birgitta/js/index_animation.js
+++ Birgitta/js/index_animation.js
@@ -1,16 +1,11 @@
 $(function(){
 
 $(".white-item").hide(); 
-$(".second-index").hide(); 
+$("#second-index").hide(); 
 $(".main-down").click(
   function(){
-  $("#item_1").slideDown(500);
-  $("#item_2").delay(100).slideDown(500);
-  $("#item_3").delay(200).slideDown(500);
-  $("#item_4").delay(300).slideDown(500);
-  $("#index").delay(350).hide(500); 
-  $(".second-index").delay(400).fadeIn(500); 
-  $(".white-item").delay(500).hide(500); 
+  $("#second-index").slideDown(500);
+
 
 });
 
