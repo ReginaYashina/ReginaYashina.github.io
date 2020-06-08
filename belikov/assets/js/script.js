800074d1a45bf5a07e3a8541e9d9cd449ee2ff54
ReginaYashina <reginayashina@inbox.ru>
2020-06-08 11:16:55 +0300

    fix main_screen

--- belikov/assets/js/script.js
+++ belikov/assets/js/script.js
@@ -11,25 +11,12 @@ function ibg(){
     }
     
     ibg();
-
-
-    //jQuery
-    // function ibg(){
-
-    //     $.each($('.ibg'), function(index, val) {
-    //     if($(this).find('img').length>0){
-    //     $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-    //     }
-    //     });
-    //     }
-        
-    //     ibg(); ;
-$(document).ready(function () {
-    $('.header--burger').click(function () {
+;
+$('.header--burger').click(function () {
 $('.header--burger, .header--nav-body').toggleClass('active');
 $('body').toggleClass('lock');
     });
-});;
+;
 //определяет, когда использовать формат изображений .webp
 function testWebP(callback) {
 
