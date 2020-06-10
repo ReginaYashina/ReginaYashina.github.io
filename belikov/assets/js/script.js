4ba6fb70de3515b7c93a63d43c30d9e0bc58fd76
ReginaYashina <reginayashina@inbox.ru>
2020-06-10 18:12:59 +0300

    add slider_section, dream_house_section, cost_section & part of  tariffs_section

--- belikov/assets/js/script.js
+++ belikov/assets/js/script.js
@@ -35,4 +35,64 @@ function testWebP(callback) {
     document.querySelector('body').classList.add('no-webp');
     }
     });;
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:1000,
+		autoplaySpeed:800,
+		appendDots:$('.slider_section--dots'),
+		responsive:[
+			{
+				breakpoint: 540,
+				settings: {
+					arrows:false
+				}
+			}
+			// {
+			// 	breakpoint: 550,
+			// 	settings: {
+			// 		slidesToShow:1
+			// 	}
+			// }
+		 ]
+	});
+// $('.slider').slick('setPosition');
+	// $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	// 	console.log(currentSlide);
+	// 	if(nextSlide === 4){
+	// 		$('.slider-section__bg').fadeIn(3000);
+	// 		$('.slick-arrow').fadeOut();
+	// 	} else {
+	// 		$('.slider-section__bg').fadeOut();
+	// 		$('.slick-arrow').fadeIn(3000);
+	// 	}
+
+	// 	if(nextSlide === 1){
+	// 		$('#img_4, #img_3, #img_2').addClass('animation');
+	// 	} else{
+	// 		$('#img_4, #img_3, #img_2').removeClass('animation');
+	// 	}
+
+	// 	if(nextSlide === 2){
+	// 		$('#img_5, #img_5-1, #img_6, #img_6-1, #img_7, #img_8').addClass('animation');
+	// 	} else{
+	// 		$('#img_5, #img_5-1, #img_6, #img_6-1, #img_7, #img_8').removeClass('animation');
+	// 	}
+
+	// 	if(nextSlide === 3){
+	// 		$('#img_9, #img_9-1, #img_10, #img_10-1').addClass('animation');
+	// 	} else{
+	// 		$('#img_9, #img_9-1, #img_10, #img_10-1').removeClass('animation');
+	// 	}
+
+
+
+
+	// });
+});
+
+;
  
